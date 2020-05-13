1cdd7fa0a4a1a269b63f07a928f72147837e1087
cheerag <chiragverma188@gmail.com>
2020-05-14 00:13:54 +0530

    django-wordCounter-app

---
+++ django-introduction-app/django_demo1/polls/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question, Choice
+
+admin.site.site_header = "Polls Admin"
+admin.site.site_title = "Polls Admin Area"
+admin.site.index_title = "Welcome to the Polls admin area"
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None,{'fields':['question_text']}),
+    ('Date Information',{'fields':['pub_date'],'classes':
+    ['collapse']}),]
+
+    inlines = [ChoiceInline]
+
+admin.site.register(Question,QuestionAdmin)
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
